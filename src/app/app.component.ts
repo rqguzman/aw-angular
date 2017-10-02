cb3a79721f7d74d2e3892aaee18eaeccc345b721
rqguzman <rafael.guzman@outlook.com>
2017-10-02 10:01:04 -0300

    app.component html & ts files changed due Template reference binding examples .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nome = 'Rafael';
-  // idade = 44;
 
-  // getIdade(){
-  //   return this.idade;
-  // }
-  adicionar(){
-    console.log(`Adicionando ${this.nome}`);
-    
-    const numero = Math.round(Math.random() * 100);
-    this.nome = 'Paulo ' + numero;
-  }
-
-  alterarNome(event: any){
-    // console.log(event);
-    
-    this.nome = event.target.value;
+  adicionar(nome: string){
+    console.log(nome);
+    this.nome = nome;
   }
 
 }
