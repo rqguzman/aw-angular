57010dba5db36d280b837faa483e290f77612df2
rqguzman <rafael.guzman@outlook.com>
2017-10-23 15:29:09 -0200

    Use of CSS styles example

--- src/app/funcionario-card/funcionario-card.component.ts
+++ src/app/funcionario-card/funcionario-card.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-funcionario-card',
   templateUrl: './funcionario-card.component.html',
-  styleUrls: ['./funcionario-card.component.css']
+  // styleUrls: ['./funcionario-card.component.css']
+  styles: [`
+    .card-block {
+      text-transform: uppercase;
+      color: blue;
+    }
+  `]
 })
 export class FuncionarioCardComponent {
   
